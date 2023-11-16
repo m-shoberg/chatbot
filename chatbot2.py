69fdbac4cbdb1f4dbdcba2cac74a9c810a33732b
Marcus Shoberg <90159393+m-shoberg@users.noreply.github.com>
2023-11-16 00:21:20 -0500

    Add files via upload

--- chatbot2.py
+++ chatbot2.py
@@ -1,11 +1,9 @@
+from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
+from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
 from transformers import pipeline
-
-'''
-This script is broke, go to chatbot2.py
-'''
+import soundfile as sf
 
 # Step 1: Speech Recognition
-# Initialize the Automatic Speech Recognition (ASR) pipeline
 asr = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
 
 def transcribe_audio(audio_file):
@@ -14,33 +12,33 @@ def transcribe_audio(audio_file):
     return asr(audio_bytes)["text"]
 
 # Step 2: Language Processing
-# Initialize the text generation pipeline
 text_generator = pipeline("text-generation", model="gpt2")
 
 def generate_response(text_input):
     return text_generator(text_input, max_length=50)[0]["generated_text"]
 
-# Step 3: Text-to-Speech
-# Initialize the TTS pipeline
-tts = pipeline("text-to-speech", model="facebook/fastspeech2-en-ljspeech")
+# Load FastSpeech2 model
+models, cfg, task = load_model_ensemble_and_task_from_hf_hub(
+    "facebook/fastspeech2-en-ljspeech",
+    arg_overrides={"vocoder": "hifigan", "fp16": False}
+)
+model = models[0]
+TTSHubInterface.update_cfg_with_data_cfg(cfg, task.data_cfg)
+generator = task.build_generator([model], cfg)
 
 def synthesize_speech(text_input):
-    return tts(text_input)
+    sample = TTSHubInterface.get_model_input(task, text_input)
+    wav, rate = TTSHubInterface.get_prediction(task, model, generator, sample)
+    return wav, rate
 
 # Main function to process speech-to-speech
 def speech_to_speech(audio_file):
-    # Convert speech to text
     text_input = transcribe_audio(audio_file)
-    
-    # Generate text response
     text_response = generate_response(text_input)
-    
-    # Convert text response to speech
-    audio_response = synthesize_speech(text_response)
-    
-    # Save or process the audio response
-    with open("response.wav", "wb") as f:
-        f.write(audio_response["stream"])
+    wav, rate = synthesize_speech(text_response)
+
+    # Save the output to a file
+    sf.write('response.wav', wav, rate)
 
     return "response.wav"
 
